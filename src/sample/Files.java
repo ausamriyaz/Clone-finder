ec7d4e2ea24956beb08a9e25bc5b21d7c9ac3d3a
Ausam Riyaz <dosfilthtab@gmail.com>
2018-04-18 01:26:45 +0530

    a

---
+++ src/sample/Files.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Files {
+    String names;
+    int totalBytes;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+}
